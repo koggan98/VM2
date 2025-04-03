5ca03eddca8ee996e489b4ae20495733870ca335
Daniel <daniel.kogan@stud.hslu.ch>
2025-04-03 17:08:09 +0200

    mit kraftwerten rumgespielt mit björn

--- src/gripper_feel/src/gripper_opener.py
+++ src/gripper_feel/src/gripper_opener.py
@@ -82,7 +82,7 @@ class SocketControllerNode(Node):
         force_y = msg.wrench.force.y
         force_z = msg.wrench.force.z
         ###########################################################################
-        if abs(force_x) > 3 or abs(force_y) > 3 or abs(force_z) > 3:
+        if abs(force_x) > 1 or abs(force_y) > 1 or abs(force_z) > 1:
         ###########################################################################
             self.get_logger().info("Force threshold exceeded, opening gripper.")
             self.ur_node.command_gripper(0, speed=255, force=1)
